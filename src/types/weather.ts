c91ae62f297bb7383867a7e4f312aa40b0fc6dc6
atmigtnca <goodmorningyb@gmail.com>
2025-09-25 11:04:49 +0900

    ver0.53

---
+++ src/types/weather.ts
@@ -0,0 +1,10 @@
+export interface WeatherFlags {
+  wet: boolean
+  feels_cold: boolean
+  muggy: boolean
+  windy: boolean
+  clear: boolean
+  hot_peak: boolean
+  cold_min: boolean
+  hot?: boolean
+}
